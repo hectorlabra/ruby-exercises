8bbe59968a4fae0c2a02987a9c7a7baa34d419d7
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2024-04-22 17:30:15 -0400

    1_data_types exercises finished

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -20,20 +20,25 @@ end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  a % b
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  (a / b).to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  string.to_i
 end
 
 def even?(number)
   # return true if the number is even (hint: use integer's even? method)
+  number.even?
 end
 
 def odd?(number)
   # return true if the number is odd (hint: use integer's odd? method)
+  number.odd?
 end
