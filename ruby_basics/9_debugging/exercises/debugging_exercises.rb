fc0fe2b7f68edac2f57ccf31120c25bfd75fa6ca
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2024-05-07 19:49:28 -0400

    9_debbugging increment greatest value finished

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -10,7 +10,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -21,16 +21,18 @@ end
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
+  greatest_value = nil
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value.nil? || current_value > greatest_value
         greatest_value = current_value
       end
     end
   end
-  greatest_value += 1
+
+  greatest_value += 1 if greatest_value
 end
+
 
 # This next exercise might look familiar
 # Use p and puts in order to find what's wrong with our method
